5b52c44c84ec3e339afb74ec06e70bbe0fb1538d
xiezc <TLqodu25>
2022-06-02 16:57:00 +0800

    vue

--- vxpt-web/src/main.js
+++ vxpt-web/src/main.js
@@ -1,11 +1,4 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from "./assets/js/router";
-// main.ts
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
-const app = createApp(App)
-app.use(router);
-app.use(ElementPlus)
-app.mount('#app')
+createApp(App).mount('#app')
